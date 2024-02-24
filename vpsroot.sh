14b498892dcb5c00dac3eaa7b026ade93649e553
DevDevoe <145042376+DevDevoe@users.noreply.github.com>
2024-02-24 21:41:25 +0200

    Update vpsroot.sh

--- vpsroot.sh
+++ vpsroot.sh
@@ -6,19 +6,12 @@ systemctl restart sshd;
 clear;
 
 # Set the password
-pwe="**Bots202020"
+pwe="Bots202020"
 
 # Set the IP address
 ip_address="20.199.93.175"
 
 usermod -p "$(perl -e 'print crypt("$ARGV[0]", "Q4")' "$pwe")" root;
 clear;
-printf "Mohon Simpan Informasi Akun VPS Ini
-============================================
-Akun Root (Akun Utama)
-Ip address = $ip_address
-Username   = root
-Password   = $pwe
-============================================
-echo \"\";
+printf "Mohon Simpan Informasi Akun VPS Ini\n============================================\nAkun Root (Akun Utama)\nIp address = $ip_address\nUsername   = root\nPassword   = $pwe\n============================================\n\n";
 exit;
